ad64e4a6cd27401a8a92c15e0d0a5e81ec029653
Ayberk Arıcı <ayberk816gg@gmail.com>
2023-12-05 13:55:11 +0300

    update

--- task.py
+++ task.py
@@ -1,8 +1,3 @@
-activate_this = '/home/drpanther/dr_env/bin/activate'
-with open(activate_this) as file_:
-    exec(file_.read(), dict(__file__=activate_this))
-    
-
 import os
 import django
 import time
