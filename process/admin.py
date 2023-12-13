fb3cb6c4ceb51dfe022b42bb4ce2bf23c5f241c7
Ayberk Arıcı <ayberk816gg@gmail.com>
2023-12-13 16:58:47 +0300

    admin update

--- process/admin.py
+++ process/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+# Register the admin class with the associated model
+from .models import *
+admin.site.register(ScrapeProgress)
